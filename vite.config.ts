79d78061989f32e3f11c1270e9479aaae1a46f6e
CornerstonesDigital <159790060+CornerstonesDigital@users.noreply.github.com>
2025-06-06 15:41:57 +0000

    chore: add initial dashboard scaffold

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // We are adding this 'css' section to explicitly
+  // tell Vite to use PostCSS and Tailwind.
+  css: {
+    postcss: './postcss.config.js',
+  },
 })
